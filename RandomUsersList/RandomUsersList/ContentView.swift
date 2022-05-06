8dc245db2608fd79e7316da1d05b52f1674a9388
MAC <miguelangelcabrera1@hotmail.com>
2022-05-06 11:14:15 +0200

    Extract user row to new view

--- RandomUsersList/RandomUsersList/ContentView.swift
+++ RandomUsersList/RandomUsersList/ContentView.swift
@@ -13,28 +13,9 @@ struct ContentView: View {
     var body: some View {
         List {
             ForEach(usersVM.randomUsers, id: \.self) { user in
-                
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("\(user.name.first), \(user.name.last)")
-                        Text("\(user.email)")
-                            .font(.footnote)
-                        
-                    }
-                    Spacer()
-                    imagesVM.image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50)
-                }.onAppear(perform: {
-                    imagesVM.getNetworkImage(url: URL(string: user.picture.thumbnail))
-                })
-                
-                    
+                UserRow(user: user)
             }
-        }.onAppear(perform: {
-            //usersVM.fetchData()
-        })
+        }
     }
 }
 
@@ -43,5 +24,27 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environmentObject(viewModel)
+    }
+}
+
+struct UserRow: View {
+    @StateObject var imagesVM = ImageViewModel()
+    let user: UserModel
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(user.name.first), \(user.name.last)")
+                Text("\(user.email)")
+                    .font(.footnote)
+                
+            }
+            Spacer()
+            imagesVM.image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50)
+        }.onAppear(perform: {
+            imagesVM.getNetworkImage(url: URL(string: user.picture.thumbnail))
+        })
     }
 }
